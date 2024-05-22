7c2a342e25eaca7c03cd289439c721c9a247af69
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-22 08:45:43 -0300

    criando a navegação

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hyrule/utils/consts/categories.dart';
+import 'package:hyrule/screens/components/category.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
@@ -6,11 +8,20 @@ class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        title: const Text("Escolha uma categoria"),
-        centerTitle: true,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Escolha uma categoria"),
+          centerTitle: true,
+        ),
+        body: GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+            ),
+            children:
+                categories.keys.map((e) => Category(category: e)).toList()),
       ),
-    ));
+    );
   }
 }
--- lib/screens/components/category.dart
+++ lib/screens/components/category.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:hyrule/screens/results.dart';
 import 'package:hyrule/utils/consts/categories.dart';
 
-class name extends StatelessWidget {
-  const name({super.key, required this.category});
+class Category extends StatelessWidget {
+  const Category({super.key, required this.category});
   final String category;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         InkWell(
-          onTap: () {},
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Results(
+                  category: category,
+                ),
+              ),
+            );
+          },
           child: Ink(
             child: Center(
               child: Image.asset("$imagePath$category.png"),
