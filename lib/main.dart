eaca0e1c11fd308a6f0f605f6c59818a0db4c625
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-31 10:37:17 -0300

    css da main

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class Hyrule extends StatelessWidget {
         brightness: Brightness.dark,
         useMaterial3: true,
       ),
-      home: Categories(),
+      home: const Categories(),
     );
   }
 }
